ba50d708175e8e43eadc6d3520337ab1b011660a
Sunstrike <sunstrike@azurenode.net>
2013-08-18 11:06:33 +0100

    Skeleton files, mcmod.info and icon

---
+++ src/io/sunstrike/mantle/Mantle.scala
@@ -0,0 +1,39 @@
+package io.sunstrike.mantle
+
+import cpw.mods.fml.common.{FMLCommonHandler, Mod}
+import cpw.mods.fml.common.network.NetworkMod
+import cpw.mods.fml.common.event.{FMLPostInitializationEvent, FMLInitializationEvent, FMLPreInitializationEvent}
+import cpw.mods.fml.common.Mod.EventHandler
+import io.sunstrike.mantle.lib.Repo._
+
+/*
+ * Mantle
+ * io.sunstrike.mantle
+ */
+
+/**
+ * Mantle Core
+ *
+ * @author Sunstrike
+ */
+@Mod(modid = "Mantle", modLanguage = "scala", name = "Mantle", version = "@VERSION@")
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
+object Mantle {
+
+    @EventHandler
+    def preInit(evt: FMLPreInitializationEvent) {
+        logger.setParent(FMLCommonHandler.instance().getFMLLogger)
+        logger.info("Mantle preinit completed.")
+    }
+
+    @EventHandler
+    def init(evt: FMLInitializationEvent) {
+        logger.info("Mantle init completed.")
+    }
+
+    @EventHandler
+    def postInit(evt: FMLPostInitializationEvent) {
+        logger.info("Mantle postinit completed.")
+    }
+
+}
---
+++ src/io/sunstrike/mantle/lib/Repo.scala
@@ -0,0 +1,19 @@
+package io.sunstrike.mantle.lib
+
+import java.util.logging.Logger
+
+/*
+ * Repo
+ * io.sunstrike.mantle.lib
+ */
+
+/**
+ * Object storeroom
+ *
+ * @author Sunstrike
+ */
+object Repo {
+
+    val logger:Logger = Logger.getLogger("Mantle")
+
+}
